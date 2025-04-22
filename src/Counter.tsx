d7f835c024b81ffffb3c9ca6f991e1b5c0831e8b
Iskandarus <35442045+Iskandarus@users.noreply.github.com>
2025-04-22 17:40:29 +0500

    Project prepearing
    Set up code styling

---
+++ src/Counter.tsx
@@ -0,0 +1,18 @@
+﻿import * as React from "react";
+import { Button } from "./components/Button/Button.tsx";
+
+export function Counter() {
+    const [count, setCount] =  React.useState(0);
+
+    const setCounterHandler = () => {
+        setCount(count + 1);
+    }
+
+    return (
+        <>
+            <Button onClick={() => {setCounterHandler()}}>
+                count is {count}
+            </Button>
+        </>
+    );
+}
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+﻿import cls from './Button.module.css';
+
+const isPrimary: boolean = true;
+
+export function Button(props) {
+    return (
+        <>
+            <button
+                className={`${cls.btn} ${isPrimary ? cls.primary : ''}`}
+                onClick={props.onClick}
+            >
+                {props.children}
+            </button>
+        </>
+    );
+}
