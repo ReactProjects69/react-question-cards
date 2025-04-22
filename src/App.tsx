06ebaa523b6274ddc9cc16ff8505be566cd7a7b8
Iskandarus <35442045+Iskandarus@users.noreply.github.com>
2025-04-22 18:55:17 +0500

    Main Layout
    Created and stylized main layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Counter } from "./Counter.tsx";
+import { MainLayout } from './components/MainLayout';
 
-function App() {
-
+export function App() {
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <hr/>
-            <Counter/>
+            <MainLayout />
         </>
-    )
+    );
 }
-
-export default App
---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,18 @@
+﻿import cls from './MainLayout.module.css';
+
+export function MainLayout() {
+    const currentYear: number = new Date().getFullYear();
+
+    return (
+        <div className={cls.mainLayout}>
+            <header>Header</header>
+            <div className={cls.mainWrapper}>
+                <main className={cls.main}>Main</main>
+                <footer className={cls.footer}>
+                    React Question Card Application | {currentYear} <br />
+                    by Alex 69
+                </footer>
+            </div>
+        </div>
+    );
+}
